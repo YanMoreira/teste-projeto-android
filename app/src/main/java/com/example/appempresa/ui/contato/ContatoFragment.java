50837f60096f20a3d856edff13f837be552a08fe
YanMoreira <yan11.moreira@gmail.com>
2020-04-10 17:22:42 -0400

    modificações no layout

--- app/src/main/java/com/example/appempresa/ui/contato/ContatoFragment.java
+++ app/src/main/java/com/example/appempresa/ui/contato/ContatoFragment.java
@@ -24,6 +24,6 @@ public class ContatoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contato, container, false);
+        return inflater.inflate(R.layout.fragment_contatos, container, false);
     }
 }
